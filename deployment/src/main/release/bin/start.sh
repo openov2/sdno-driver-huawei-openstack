b15b810db772391efb933a42310d3bc3136ba76a
angilecoder <zhaoyangzhao.zhaoyang@huawei.com>
2016-10-12 11:09:14 +0800

    Fix deploy project
    Fix start and stop script
    
    SDNO-583
    
    Change-Id: Ic3650a29ee2032e0782d7c13a81b7cd065715073
    Signed-off-by: angilecoder <zhaoyangzhao.zhaoyang@huawei.com>

--- deployment/src/main/release/bin/start.sh
+++ deployment/src/main/release/bin/start.sh
@@ -18,32 +18,4 @@
 
 cd ..
 export CATALINA_BASE=$(cd `dirname $0`; pwd)
-
-if [ -z "$JAVA_HOME" ]
-then
-    echo "There is no JAVA_HOME"
-    exit 1
-fi
-
-if [ -z "$CATALINA_HOME" ]
-then
-    echo "There is no CATALINA_HOME"
-    exit 1
-fi
-
-if [ -z "$CATALINA_BASE" ]
-then
-    echo "There is no CATALINA_BASE"
-    exit 1
-fi
-
-export CATALINA_PID="$CATALINA_BASE/bin/catalina.pid"
-
-LOG_DIR=$CATALINA_BASE/logs
-if [ ! -d "$LOG_DIR" ]; then
-  mkdir $LOG_DIR
-fi
-
-$CATALINA_HOME/bin/catalina.sh start
-
-
+catalina.sh start
